dfea044ea0dd5e430c133e09e04016210fa8f7be
Prospertech-github <ikechukwuprosper8@gmail.com>
2025-03-10 14:05:51 +0100

    feat: completed form styling

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./form.module.css";
 import MyLogo from "../../images/microsoft-logo.png";
 import { FaKey } from "react-icons/fa";
 
 const Form = () => {
+  const [viewPassword, setViewPassword] = useState(true);
+
   return (
     <div className={styles.formDiv}>
       <div className={styles.formCtn}>
@@ -13,34 +15,29 @@ const Form = () => {
         </div>
 
         <form action="">
-          <h1>Sign in</h1>
+          
+          <h1>{!viewPassword ? 'Sign in' : 'Enter Password'}</h1>
 
           <div>
             <input
-              type="email number skype"
-              name=""
-              id=""
-              placeholder="Email, Phone or Skype"
+              type={!viewPassword ? 'email' : 'password'}
+              name={!viewPassword ? 'email' : 'password'}
+              id={!viewPassword ? 'email' : 'password'}
+              placeholder={!viewPassword ? "Email, Phone or Skype" : "Password"}
             />
           </div>
 
-          <p>&#65343;</p>
 
-          <div>
-            <p>No account? <span>Create one</span></p>
-            <p>Cant access your account?</p>
+          <div className={styles.formBtmDiv}>
+            <p role="button">{!viewPassword ? 'No account?' : ''} <span className={styles.colorP}>{!viewPassword ? 'Create one!' : 'Forgot password?'}</span></p>
+            <p className={styles.colorP} role="button"> {!viewPassword ? "Can't access your account?" : 'Email code to ikechukwuprosper8@gmail.com'}</p>
           </div>
 
-          <div>
-            <button type="button">Back</button>
-            <button type="submit">Next</button>
+          <div className={styles.btnDiv}>
+            {/* <button type="button">Back</button> */}
+            <button type="submit">{!viewPassword ? 'Next' : 'Sign In'}</button>
           </div>
         </form>
-
-        <div>
-          <FaKey />
-          <p>Sign in-options</p>
-        </div>
       </div>
     </div>
   );
